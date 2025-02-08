c0651de14e4e28a0dae610c3e5456344e8a1ba1f
LCD-PC <dan.luongcong@gameloft.com>
2025-02-08 09:48:48 +0700

    + connect relay main menu

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -13,34 +13,100 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField]
     private GameObject loadingPannel;
 
+    [SerializeField]
+    private GameObject waitingPannel;
+
+    [SerializeField]
+    private bool useRelay = false;
+
+    [SerializeField]
+    private string joinCode;
+
     void Awake()
     {
-        buttonHost.onClick.AddListener(OnHostClicked);
-        buttonClient.onClick.AddListener(OnClientClicked);
+        if (useRelay)
+        {
+            buttonHost.onClick.AddListener(OnRelayHostClicked);
+            buttonClient.onClick.AddListener(OnRelayClientClicked);
+        }
+        else
+        {
+            buttonHost.onClick.AddListener(OnHostClicked);
+            buttonClient.onClick.AddListener(OnClientClicked);
+        }
         loadingPannel.SetActive(false);
+        waitingPannel.SetActive(false);
+    }
+
+    private void ShowLoading()
+    {
+        loadingPannel.SetActive(true);
+        waitingPannel.SetActive(false);
+    }
+
+    private void ShowWaiting()
+    {
+        loadingPannel.SetActive(false);
+        waitingPannel.SetActive(true);
+    }
+
+    private void HideButtons()
+    {
+        buttonHost.gameObject.SetActive(false);
+        buttonClient.gameObject.SetActive(false);
     }
 
     private void OnClientClicked()
     {
+        HideButtons();
+        ShowLoading();
         bool success = NetworkManager.Singleton.StartClient();
         if (success)
         {
-            loadingPannel.SetActive(true);
-
-            buttonHost.gameObject.SetActive(false);
-            buttonClient.gameObject.SetActive(false);
+            ShowWaiting();
         }
     }
 
     private void OnHostClicked()
     {
+        HideButtons();
+        ShowLoading();
         bool success = NetworkManager.Singleton.StartHost();
         if (success)
         {
-            loadingPannel.SetActive(true);
+            ShowWaiting();
+        }
+    }
 
-            buttonHost.gameObject.SetActive(false);
-            buttonClient.gameObject.SetActive(false);
+    private async void OnRelayHostClicked()
+    {
+        HideButtons();
+        ShowLoading();
+        joinCode = await RelayNetworkManagerSingleton.Instance.StartHostWithRelay();
+        if (!string.IsNullOrEmpty(joinCode))
+        {
+            Debug.Log($"Join code: {joinCode}");
+            ShowWaiting();
+        }
+        else
+        {
+            Debug.LogError("Failed to start host");
+        }
+    }
+
+    private async void OnRelayClientClicked()
+    {
+        HideButtons();
+        ShowLoading();
+        bool success = await RelayNetworkManagerSingleton.Instance.StartClientWithRelay(joinCode);
+        if (success)
+        {
+            Debug.Log("Connected to host");
+            ShowWaiting();
+        }
+        else
+        {
+            Debug.LogError("Failed to connect to host");
         }
     }
 }
